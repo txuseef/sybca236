f692c3e24ba36a438bc9a8331f2849dfdd2cf1cd
tauseef shaikh <tauseef.shaikh8625@gmail.com>
2024-01-18 10:52:01 +0530

    commit

---
+++ github/src/github/addd.java
@@ -0,0 +1,17 @@
+package github;
+
+public class addd {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int x=10;
+		int y=4;
+		int z=x+y;
+		int c=x-y;
+		System.out.println("The sum="+ z);
+		System.out.println("The difference="+ c);
+
+	}
+
+}
