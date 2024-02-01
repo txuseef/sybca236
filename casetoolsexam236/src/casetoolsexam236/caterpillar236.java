2c3689ba842dba8d1c50d7f7572d80a19be0e34a
tauseef shaikh <tauseef.shaikh8625@gmail.com>
2024-02-01 10:46:11 +0530

    commit first

---
+++ casetoolsexam236/src/casetoolsexam236/caterpillar236.java
@@ -0,0 +1,13 @@
+package casetoolsexam236;
+
+public class caterpillar236 {
+	
+	int moves,cabbages;
+	
+	caterpillar236(int moves, int cabbages)
+	{
+		this.moves=moves;
+		this.cabbages=cabbages;
+	}
+
+}
